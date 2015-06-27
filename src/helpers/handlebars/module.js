b0e4091f6fc60993d2b1ba6e8b25eb0dc9720b30
Eugeny Dementev <e.dementev@2gis.ru>
2015-06-27 15:56:27 +0600

    fix context for templates

--- src/helpers/handlebars/module.js
+++ src/helpers/handlebars/module.js
@@ -1,7 +1,9 @@
 import _ from 'lodash';
 
-export default function moduleHelper(moduleName, { fn }) {
-    var moduleInstance = _.find(this._modules, module => _.eq(module.type, moduleName));
+export default function moduleHelper(moduleName, { fn, data: { module } }) {
+    var context = this;
+
+    var moduleInstance = _.find(module._modules, instance => _.eq(instance.type, moduleName));
 
     if (!moduleInstance) {
         throw new TypeError(`Module ${moduleName} are not child`);
